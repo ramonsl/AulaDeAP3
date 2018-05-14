4fd3aaabe6dcb6323e1aed397f56a798218c52f7
ramonsl <ramonsl@gmail..com>
2018-05-14 15:51:11 -0300

    Aula13
    Uso de Classes e métodos.
    Exemplo conta Bancaria.
    Exemplo Livros.
    Exemplo Classe Bomba

---
+++ src/Cliente.java
@@ -0,0 +1,7 @@
+public class Cliente {
+    public int Cpf;
+    public String Nome;
+    public int Idade;
+
+
+}
---
+++ src/TestaBomba.java
@@ -0,0 +1,10 @@
+public class TestaBomba {
+
+
+    public static void main(String[] args) {
+        Bomba b1= new Bomba();
+
+
+        b1.ligar(10);
+    }
+}
