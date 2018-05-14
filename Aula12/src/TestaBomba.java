393b0b6b5a04cf21cd8276f3a2a14429da7f28ec
ramonsl <ramonsl@gmail..com>
2018-05-14 15:58:05 -0300

    Removendo_copias
    Removendo Copias

--- Aula12/src/TestaBomba.java
+++ Aula12/src/TestaBomba.java
@@ -3,7 +3,7 @@ public class TestaBomba {
 
     public static void main(String[] args) {
         Bomba b1= new Bomba();
-
+///hjgg
 
         b1.ligar(10);
     }
--- src/Cliente.java
+++
@@ -1,7 +0,0 @@
-public class Cliente {
-    public int Cpf;
-    public String Nome;
-    public int Idade;
-
-
-}
